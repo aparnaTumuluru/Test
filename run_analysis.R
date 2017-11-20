a0e1722d8caaf344f4a94c19f9d5837d0e767aa0
aparnaTumuluru <32533756+aparnaTumuluru@users.noreply.github.com>
2017-11-20 14:14:36 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -4,17 +4,18 @@ setwd("D:\\DataScienceRepo\\Quiz4\\Q4")
 run_analysis<-function()
   {
   filename <- "HAR_dataset.zip"
+  ########uncomment the below line if file is not present in working directory
   #download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",filename)
-  ##unizp contents
+  
   if(file.exists(filename)){
-   # unzip(filename)
+   ########uncomment the below line if file is not present in working directory
+   # unzip(filename)    
     
+    #Read activity names and Features
+    activity_labels<-read.table("UCI HAR Dataset/activity_labels.txt")    
+    features<-read.table("UCI HAR Dataset/features.txt")
     
-    activity_labels<-read.table("UCI HAR Dataset/activity_labels.txt")
-    
-    features<-read.table("UCI HAR Dataset\\features.txt")
-    
-    #Extract only the measurements on the mean and standard deviation for each measurement.
+    #Extract only the features with mean and standard deviation.
     selectedFeatures<- grep("mean|std",features[,2])
     
     #read training details
@@ -30,27 +31,30 @@ run_analysis<-function()
     testTable <-cbind(test_subjects,test_activities,test_details)
     
     #merge the Test and training data 
-    
     fullData<-rbind(trainTable,testTable)
+     #get names of features with Indexes from selectedFeatures
     featureNames<-features[selectedFeatures,2]
-    featureNames<- gsub("\\()","" ,featureNames)
     
+     #remove all special characters
+    featureNames<- gsub("\\()","" ,featureNames)
+    featureNames<- gsub("-","" ,featureNames)
+    
+    #set column names 
     colnames(fullData)<-c("Subjects","activities",featureNames)
     
     #add activity labels  
     fullData$activities <- factor(fullData$activities, levels = activity_labels[,1], labels = activity_labels[,2])
-    
-    library(tidyr)
-    #tidy data 
+        
+     #tidy data - merge all the feature names into a single column and its values to another.
     tidyData<- gather(fullData,3:81,Values,-Subjects,-activities)
-      colnames(tidyData)[3]<-"Variables"
-    #Mean values group wise
-      library(dplyr)
-      tidyDataGrouped<-  group_by(tidyData,Subjects,activities,Variables)
-     summarize(tidyDataGrouped,mean(Values) )
-      
-      #write.table(result,"result.txt",row.names =FALSE )
-                       
+    colnames(tidyData)[3]<-"Variables"
+    
+    #Mean values group wise     
+     tidyDataGrouped<- group_by(tidyData,Subjects,activities,Variables)
+     summarize(tidyDataGrouped,mean(Values))
+    
+     write.table(result,"result.csv",row.names =FALSE,sep = "," )
+                  
     
   }
   
